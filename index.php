e5aca9dc821dfeabb6a62de16a4b795b43c8c805
0505Keitan <0505Keitan@gmail.com>
2019-01-28 13:12:45 +0900

    説明文追加

--- index.php
+++ index.php
@@ -3,10 +3,16 @@
     <meta charset="UTF-8">
     <title>デジタルコンテンツ制作同好会</title>
     <link rel="stylesheet" href="front/index.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 </head>
 <body>
+<div class="container">
 <div align="center">
     <h1>デジタルコンテンツ制作同好会</h1>
+    <br>
+    <p>はいどうも〜！デジコンことデジタルコンテンツ制作同好会で〜す！</p>
+    <p>よろしくぅ！！</p>
+</div>
 </div>
 </body>
 </html>
