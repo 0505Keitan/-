e50d61c004dfe2b2d553841949f4d663f9dc339e
0505Keitan <0505Keitan@gmail.com>
2019-01-28 13:05:26 +0900

    トップページ作成

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>デジタルコンテンツ制作同好会</title>
+    <link rel="stylesheet" href="front/index.css">
+</head>
+<body>
+<div align="center">
+    <h1>デジタルコンテンツ制作同好会</h1>
+</div>
+</body>
+</html>
